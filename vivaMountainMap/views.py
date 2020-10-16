2a80ef97673dc6d0c8987308366a907b22a68326
Junhee Cho <junhee3085@gmail.com>
2020-10-16 14:19:19 +0900

    test folium map

--- vivaMountainMap/views.py
+++ vivaMountainMap/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import folium
 
 # Create your views here.
 
@@ -6,7 +7,14 @@ def main(requests):
     pass
 
 def mountain_map(requests):
-    pass
+    lat_long = [35.3369, 127.7306]
+    m = folium.Map(lat_long, zoom_start=10)
+    popText = folium.Html('<b>Jirisan</b><br>' + str(lat_long), script=True)
+    popup = folium.Popup(popText, max_width=2650)
+    folium.RegularPolygonMarker(location=lat_long, popup=popup).add_to(m)
+    m = m._repr_html_()
+    datas = {'mountain_map':m}
+    return render(requests, 'maps/showmapwithfolium.html', context=datas)
 
 def info(request):
     pass
