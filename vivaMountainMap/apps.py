2db8946d267c8b55c1d44cf28eb7702222ec29b7
cho <crh8598@gmail.com>
2020-10-16 17:39:36 +0900

    fix views.py

--- vivaMountainMap/apps.py
+++ vivaMountainMap/apps.py
@@ -1,11 +1,4 @@
 from django.apps import AppConfig
-import sys
-from django.apps import AppConfig
-from .views import get_info
+
 class VivamountainmapConfig(AppConfig):
     name = 'vivaMountainMap'
-
-    def ready(self):
-            get_info()
-
-
