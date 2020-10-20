fae95fe02cf0f180e615ff7f9b51644386109904
Junhee Cho <junhee3085@gmail.com>
2020-10-20 13:35:20 +0900

    add job map init

--- jobMap/views.py
+++ jobMap/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+import folium
+from pymongo import MongoClient
 
 # Create your views here.
+
+
+def job_map(requests):
+    return render(requests, 'jobMap/job_map.html', context=datas)
+
