da2e886400c70bbeb6bb8766d88072150652078e
Junhee Cho <junhee3085@gmail.com>
2020-10-20 19:03:31 +0900

    add view job_map_search

--- jobMap/views.py
+++ jobMap/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from jobMap.jobAPI import jobAPI
 
 # Create your views here.
 
-
-def job_map(requests):
-    pass
+def job_map_search(requests):
+    if len(requests.GET) !=0:
+        keyword = requests.GET['key']
+    else:
+        keyword = 'AI'
+    job = jobAPI()
+    m = job.get_searchmap(keyword)
+    datas = {'mountain_map':m}
+    return render(requests, 'jobMap/job_map_search.html', context=datas)
